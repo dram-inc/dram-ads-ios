ca2bb1bfbbbd2d96762fe08966459ac937b05e2b
khorenDram <khoren@dram.studio>
2023-09-16 21:16:08 +0400

    clean up

---
+++ DramAds2/DramAds/Classes/TestFile.swift
@@ -0,0 +1,20 @@
+//
+//  TestFile.swift
+//  DramAds
+//
+//  Created by Khoren Asatryan on 16.09.23.
+//
+
+import Foundation
+
+public class Test {
+    
+    public class func prrrr(_ st: String) {
+        print(st)
+    }
+    
+    func hello()  {
+        
+    }
+    
+}
