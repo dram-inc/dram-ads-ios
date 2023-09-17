dce213cc2ffa1023759e9ca76839e8445854de35
khorenDram <khoren@dram.studio>
2023-09-17 18:56:49 +0400

    pod

---
+++ Example/Pods/Target Support Files/dram-ads-ios/dram-ads-ios-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_dram_ads_ios : NSObject
+@end
+@implementation PodsDummy_dram_ads_ios
+@end
---
+++ dram-ads-ios/Classes/Test2.swift
@@ -0,0 +1,16 @@
+//
+//  Test2.swift
+//  dram-ads-ios
+//
+//  Created by Khoren Asatryan on 17.09.23.
+//
+
+import Foundation
+
+public class Test {
+    
+    public class func test(_ st: String) {
+        print(st)
+    }
+    
+}
