5126aea8bfd852ffae6fd82096fd63a9a39f2c89
khorenDram <khoren@dram.studio>
2023-09-17 19:32:04 +0400

    pod

---
+++ DramAds/Classes/File.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  DramAds
+//
+//  Created by Khoren Asatryan on 17.09.23.
+//
+
+import Foundation
+
+public class Test {
+    
+    public class func pp(_ st: String) {
+        print(st)
+    }
+    
+}
