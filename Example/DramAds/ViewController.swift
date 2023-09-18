55137ede2b27f77e01c6228c2969e6134e9a3c38
khorenDram <khoren@dram.studio>
2023-09-19 01:42:03 +0400

    pod commit

--- Example/DramAds/ViewController.swift
+++ Example/DramAds/ViewController.swift
@@ -14,15 +14,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        Test.pp("rerfcvwfv")
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 }
 
